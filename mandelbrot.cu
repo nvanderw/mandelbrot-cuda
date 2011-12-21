b0aa22563561bf4354a98d627c7d446691dd1dbf
Nick Vanderweit <nicholas.vanderweit@colorado.edu>
2011-12-20 22:44:37 -0700

    Can now generate very large images, with good smooth colors.

--- mandelbrot.cu
+++ mandelbrot.cu
@@ -1,33 +1,35 @@
 #include <stdio.h>
-extern "C" int *create_mandelbrot(int res_x, int res_y, float min_x, float min_y,
+#define ABS(X) X < 0 ? -X : X
+
+
+extern "C" float *create_mandelbrot(int res_x, int res_y, float min_x, float min_y,
         float max_x, float max_y, int iter);
 
-__global__ void mandelbrot(int *region, int offset, int2 res, float4 boundary, int iter);
-__device__ int divergence(double2 c, int iteger);
+__global__ void mandelbrot(float *region, int offset, int2 res, float4 boundary, int iter);
 
 
 /* We define a maximum number of iterations the kernel can do, so that
  * it isn't rudely interrupted by the watchdog timer */
 static const long max_iterations = 1L<<30;
-static const int block_size = 256;
+static const int block_size = 128;
 
-extern "C" int *create_mandelbrot(int res_x, int res_y, float min_x, float min_y,
+extern "C" float *create_mandelbrot(int res_x, int res_y, float min_x, float min_y,
         float max_x, float max_y, int iter) {
 
-    int *region, *d_region;
+    float *region, *d_region;
     dim3 threadsPerBlock(block_size);
     dim3 blocks;
     float4 boundary;
 
     int size = res_x * res_y;
-    size_t host_bytes = (size_t) size * sizeof(int);
+    size_t host_bytes = (size_t) size * sizeof(float);
     size_t job_bytes;
     size_t jobsize;
     cudaStream_t copy_stream; // Stream for copying between host and device
     if(cudaStreamCreate(&copy_stream) != cudaSuccess)
         return NULL;
 
-    region = (int*) malloc(host_bytes);
+    region = (float*) malloc(host_bytes);
     if(!region)
         goto cleanup3;
 
@@ -44,7 +46,7 @@ extern "C" int *create_mandelbrot(int res_x, int res_y, float min_x, float min_y
             "Number of pixels per job", jobsize);
 
     blocks = dim3(jobsize / threadsPerBlock.x);
-    job_bytes = jobsize * sizeof(int);
+    job_bytes = jobsize * sizeof(float);
 
     boundary.x = min_x;
     boundary.y = min_y;
@@ -77,36 +79,28 @@ cleanup3:
     return NULL;
 }
 
-/* Tests the divergence of some given point a + b*i in the complex plane.
- *
- * Return value: if divergence was detected, the number of iterations in which
- * it occurred. Otherwise, iter is returned.
- */
-__device__ int divergence(double2 c, int iter) {
-    int i;
-    double2 z = c;
-    for(i=1;(i < iter) && (z.x < 2) && (z.y < 2);i++) {
-        z = make_double2(z.x * z.x - z.y*z.y + c.x,
-                         2*z.x*z.y + c.y);
-    }
-    return i;
-}
 
 /* Calculates the mandelbrot set and stores the results in region, which
  * should be of length gridDim.x * blockDim.x * gridDim.y * blockDim.y
  * The float4 boundary specifies the region of the complex plane to test
  * for divergence, with the x, y, z, and w components representing the minimum
  * x, minimum y, maximum x, and maximum y values of the rectangular region. */
-__global__ void mandelbrot(int *region, int offset, int2 res, float4 boundary, int iter) {
+__global__ void mandelbrot(float *region, int offset, int2 res, float4 boundary, int iter) {
     int index = blockIdx.x * blockDim.x + threadIdx.x;
     int pixel = index + offset;
     int pixel_x = pixel % res.x;
     int pixel_y = pixel / res.x;
 
-    double2 z;
+    double2 c, z;
+    int i;
 
-    z.x = ((boundary.z - boundary.x) * (((double) pixel_x) + 0.5))/(res.x) + boundary.x;
-    z.y = ((boundary.y - boundary.w) * (((double) pixel_y) + 0.5))/(res.y) + boundary.w;
+    c.x = ((boundary.z - boundary.x) * (((double) pixel_x) + 0.5))/(res.x) + boundary.x;
+    c.y = ((boundary.y - boundary.w) * (((double) pixel_y) + 0.5))/(res.y) + boundary.w;
+    z = c;
 
-    region[index] = divergence(z, iter);
+    for(i=1;(i<iter) && (z.x*z.x+z.y*z.y <= 4);i++)
+        z = make_double2(z.x * z.x - z.y*z.y + c.x,
+                         2*z.x*z.y + c.y);
+
+    region[index] = (i >= iter) ? (-1) : i + 2 - log2f(log2f(z.x*z.x+z.y*z.y));
 }
--- mandelbrot.h
+++ mandelbrot.h
@@ -2,6 +2,6 @@
 #define _MANDELBROT_H_
 #include <stdio.h>
 
-int *create_mandelbrot(int res_x, int res_y, float min_x, float min_y,
+float *create_mandelbrot(int res_x, int res_y, float min_x, float min_y,
         float max_x, float max_y, int iter);
 #endif
